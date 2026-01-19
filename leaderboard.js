07c0fee7d8505f9e0e53e2c430f0fc69f359eaff
Josh <jbgerloff@hotmail.com>
2026-01-19 17:46:57 -0500

    Update leaderboard.js

--- leaderboard.js
+++ leaderboard.js
@@ -1,1 +1,21 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
+import { getFirestore, collection, onSnapshot } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
 
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+onSnapshot(query(collection(db, 'scores'), orderBy('total', 'desc')), snap => {
+  const tableBody = document.getElementById('table-body');
+  tableBody.innerHTML = '';
+  snap.forEach((d, idx) => {
+    const tr = document.createElement('tr');
+    tr.innerHTML = `<td>${idx+1}</td><td>${d.data().entryName}</td><td>${d.data().total}</td><td>${d.data().tiebreaker}</td>`;
+    tr.onclick = async () => {
+      const bracket = (await getDoc(doc(db, 'brackets', d.id))).data();
+      document.getElementById('details').textContent = JSON.stringify(bracket.picks, null, 2);
+    };
+    tableBody.appendChild(tr);
+  });
+});
+
+// PDF export similar to admin
